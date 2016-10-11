8198ad1275cd3da3c6c7ab81e1cf44049cb6ab9a
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-10-11 19:53:00 -0400

    cssssssy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,4 @@
 
-/*=============================================================
-    Authour URI: www.binarycart.com
-    Version: 1.1
-    License: MIT
-    
-    http://opensource.org/licenses/MIT
-
-    100% To use For Personal And Commercial Use.
-   
-    ========================================================  */
-
 (function ($) {
     "use strict";
     var mainApp = {
